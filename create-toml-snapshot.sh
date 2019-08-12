24265836bef8cffacb7c9def4995e4d11a0ffac7
Toshiaki Maki <tmaki@pivotal.io>
2019-08-13 00:38:07 +0900

    Make builder lightweight

--- create-toml-snapshot.sh
+++ create-toml-snapshot.sh
@@ -4,12 +4,8 @@ VERSION=1.0.0-BUILD-SNAPSHOT
 
 buildpacks="\
 build-system-buildpack,https://repo.spring.io/libs-snapshot-local/org/cloudfoundry/buildsystem/org.cloudfoundry.buildsystem/${VERSION}/org.cloudfoundry.buildsystem-${VERSION}.tgz \
-debug-buildpack,https://repo.spring.io/libs-snapshot-local/org/cloudfoundry/debug/org.cloudfoundry.debug/${VERSION}/org.cloudfoundry.debug-${VERSION}.tgz \
-jmx-buildpack,https://repo.spring.io/libs-snapshot-local/org/cloudfoundry/jmx/org.cloudfoundry.jmx/${VERSION}/org.cloudfoundry.jmx-${VERSION}.tgz \
 jvm-application-buildpack,https://repo.spring.io/libs-snapshot-local/org/cloudfoundry/jvmapplication/org.cloudfoundry.jvmapplication/${VERSION}/org.cloudfoundry.jvmapplication-${VERSION}.tgz \
 openjdk-buildpack,https://repo.spring.io/libs-snapshot-local/org/cloudfoundry/openjdk/org.cloudfoundry.openjdk/${VERSION}/org.cloudfoundry.openjdk-${VERSION}.tgz \
-azure-application-insights-buildpack,https://repo.spring.io/libs-snapshot-local/org/cloudfoundry/azureapplicationinsights/org.cloudfoundry.azureapplicationinsights/${VERSION}/org.cloudfoundry.azureapplicationinsights-${VERSION}.tgz \
-google-stackdriver-buildpack,https://repo.spring.io/libs-snapshot-local/org/cloudfoundry/googlestackdriver/org.cloudfoundry.googlestackdriver/${VERSION}/org.cloudfoundry.googlestackdriver-${VERSION}.tgz \
 "
 
 cp java-buildpack-group/builder.toml ./
